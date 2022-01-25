feeadcbbc1f2a9b7b1028d21c2be4270ba9911cd
ArHiFranki <franki550@gmail.com>
2022-01-25 12:57:43 +0700

    Добавил скрипт для определения коллизий между игроком и стенами

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -8,9 +8,13 @@ public class Mover : MonoBehaviour
 
     private void Update()
     {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-
         transform.Translate(xValue, 0, zValue);
     }
 }
